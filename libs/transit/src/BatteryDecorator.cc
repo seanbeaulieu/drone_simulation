c9566ea8fb8b1dfa3cf7c17d5c4167086e7ec5cc
H T Gauerke <gauer045@umn.edu>
2022-12-12 22:14:17 -0600

    battery logic wip

--- libs/transit/src/BatteryDecorator.cc
+++ libs/transit/src/BatteryDecorator.cc
@@ -1,7 +1,7 @@
 #include "BatteryDecorator.h"
 
 BatteryDecorator :: BatteryDecorator(Drone* drone) {
- this->drone = drone;
+	this->drone = drone;
 }
 
 BatteryDecorator :: BatteryDecorator(JsonObject& obj) {
@@ -14,17 +14,13 @@ BatteryDecorator :: ~BatteryDecorator(){
 	}
 }
 
-//don't need it, maybe
-// void BatteryDecorator::SetDrone(Drone* drone){
-
-// }
-
 void BatteryDecorator::GetNearestEntity(std::vector<IEntity*> scheduler){
     drone->GetNearestEntity(scheduler);
 }
 
 void BatteryDecorator::Update(double dt, std::vector<IEntity*> scheduler){
-//FIXME: not working right now with drone factory.
+// TODO: Implement logic
+/*
     position = this->GetPosition();
     if (charge > 0){
        drone->Update(dt, scheduler); 
@@ -33,5 +29,43 @@ void BatteryDecorator::Update(double dt, std::vector<IEntity*> scheduler){
        charge -= dt;
        std::cout<<"charge is: "<<charge<<std::endl; 
     }
+*/
 
+	// note: may need a way to broadcast a dead battery to emergency pickup?
+	
+	// current implementation allows battery failure due to the mobile recharge station,
+	// which is both less difficult on us and can actually showcase the pickup
+	
+	if (charge <= 0) {
+		return;
+	}
+	
+	if (this->GetAvailability()) { // no target, no moving
+		drone.Update(dt, scheduler); // try to initialize drone route
+		if (!(this->GetAvailability())) { // if successful (results in 1 move)
+			charge -= dt;
+			double totalDist = 0;
+			IEntity* robot = drone->RetNearestEntity();
+			totalDist += this->GetPosition().Distance(this->GetDestination());
+			totalDist += this->GetDestination().Distance(robot->GetDestination());
+			//  total_dist += robot->GetDestination.Distance(nearestRecharge->GetPosition()); // relies on nearestRecharge, not fully implemented yet
+			if ((total_dist / speed) > (charge)) { // (DOUBLE CHECK MATH) if traveling total distance requires more time moving than the charge holds
+				rechargeStrategy = new BeelineStrategy(this->GetPosition(), nearestRecharge->GetPosition());
+			}
+		}
+	}
+	else if (rechargeStrategy) { // pathing to recharge. deleted and NULL when completed
+		// move to nearestRecharge, recalc if the station is mobile
+		if (nearIsMobile) {
+			// recalc then move
+		}
+		else {
+			rechargeStrategy->Move(this, dt);
+		}
+	}
+	else {
+		drone.Update(dt, scheduler);
+		charge -= dt;
+	}
+	
 } 

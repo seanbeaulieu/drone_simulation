c422779ab2b8d1a84b11e17e71606d3fe72ae0c9
beaul116 <beaul116@umn.edu>
2022-12-15 20:52:57 -0600

    ientity emergencypickup doxy

--- libs/transit/include/IEntity.h
+++ libs/transit/include/IEntity.h
@@ -21,24 +21,157 @@ class IEntity {
     currentId++;
   }
   virtual ~IEntity() {}
+
+/**
+ * @brief Returns the ID of the entity
+ * 
+ * @param None
+ * @return an integer ID
+ **/
   virtual int GetId() const { return id; }
+
+/**
+ * @brief Returns the current position of the entity as a Vector3 object
+ * 
+ * @param None
+ * @return Vector 3 object
+ **/
   virtual Vector3 GetPosition() const = 0;
+
+/**
+ * @brief Returns the current direction of the entity as a Vector3 object
+ * 
+ * @param None	
+ * @return Vector 3 object
+ **/
   virtual Vector3 GetDirection() const = 0;
+
+/**
+ * @brief Returns the current destination of the entity as a Vector3 Object
+ * 
+ * @param None
+ * @return Vector 3 object
+ **/
   virtual Vector3 GetDestination() const = 0;
+
+/**
+ * @brief Returns the details of the entity
+ * 
+ * @param None
+ * @return details as a JsonObject
+ **/
   virtual JsonObject GetDetails() const = 0;
+
+/**
+ * @brief Returns the current speed of the entity
+ * 
+ * @param None
+ * @return a float value representing the current speed
+ **/
   virtual float GetSpeed() const = 0;
+
+/**
+ * @brief Returns the current availability of the entity
+ * 
+ * @param None
+ * @return boolean value true or false 
+ **/
   virtual bool GetAvailability() const {};
+
+/**
+ * @brief Returns the current strategy name of the entity
+ * 
+ * @param None
+ * @return String representing the strategies name
+ **/
   virtual std::string GetStrategyName(){}
+
+/**
+ * @brief Sets the current availability of the entity
+ * 
+ * @param choice, a boolean value
+ * @return None
+ **/
   virtual void SetAvailability(bool choice) {}
+
+/**
+ * @brief Updates the entity
+ * 
+ * @param dt, a double delta value
+ * @param scheduler, a vector array of IEntity ptrs 
+ * @return None
+ **/
   virtual void Update(double dt, std::vector<IEntity*> scheduler) {}
+  
+/**
+ * @brief Sets the graph of the entity
+ * 
+ * @param IGraph graph
+ * @return None
+ **/
   virtual void SetGraph(const IGraph* graph) { this->graph = graph; }
+  
+/**
+ * @brief Sets the current position of the entity
+ * 
+ * @param pos_, a vector3 value
+ * @return None
+ **/
   virtual void SetPosition(Vector3 pos_) {}
+  
+/**
+ * @brief Sets the current direction of the entity
+ * 
+ * @param dir, a vector3 value
+ * @return None
+ **/
   virtual void SetDirection(Vector3 dir_) {}
+  
+/**
+ * @brief Sets the current destination of the entity
+ * 
+ * @param des, a vector3 value
+ * @return None
+ **/
   virtual void SetDestination(Vector3 des_) {}
+  
+/**
+ * @brief Sets the strategy of the Entity
+ * 
+ * @param strategyName, string name of the strategy
+ * @return None
+ **/
   virtual void SetStrategyName(std::string strategyName_){}
+  
+/**
+ * @brief Rotates the entity
+ * 
+ * @param angle, a double amount to rotate
+ * @return None
+ **/
   virtual void Rotate(double angle) {}
+  
+/**
+ * @brief Jumps the entity
+ * 
+ * @param height, a double amount to elevate into the air
+ * @return None
+ **/
   virtual void Jump(double height) {}
+  
+/**
+ * @brief Sets the entities into a vector array of IEntitys
+ * 
+ * @param entities, a vector array of IEntity ptrs
+ * @return None
+ **/
   virtual void SetEntities(std::vector<IEntity*>* entities) {}
+  
+/**
+ * @brief 
+ * 
+ * @return None
+ **/
   virtual bool GetEmergency() {};
  protected:
   int id;

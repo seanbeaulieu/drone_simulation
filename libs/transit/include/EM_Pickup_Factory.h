17cbcc3a5f6b25b655cc09148dbd2c265da94d0b
beaul116 <beaul116@umn.edu>
2022-12-16 12:09:43 -0600

    em factory doxy

--- libs/transit/include/EM_Pickup_Factory.h
+++ libs/transit/include/EM_Pickup_Factory.h
@@ -9,6 +9,13 @@
 class EM_Pickup_Factory : public IEntityFactory {
   public:
     virtual ~EM_Pickup_Factory() {}
+
+    /**
+     * @brief Creates a new entity as an Emergency Pickup Drone
+     * 
+     * @param entity, a memory address to a JsonObject
+     * @return a pointer to the newly created Emergency Pickup Drone
+     **/
     IEntity* CreateEntity(JsonObject& entity);
 };
 

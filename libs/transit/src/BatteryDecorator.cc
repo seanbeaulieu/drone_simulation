7245e777972cf30787590c0ecc0b75218b7bd81e
Jimmy Xiao <xiao0261@umn.edu>
2022-12-16 12:57:42 -0600

    change in batteryD_2

--- libs/transit/src/BatteryDecorator.cc
+++ libs/transit/src/BatteryDecorator.cc
@@ -58,66 +58,38 @@ void BatteryDecorator::Update(double dt, std::vector<IEntity*> scheduler){
 // 1. pathing to the mobile recharge station
 // 2. straight-line distance calculation for route length
 // which is both less difficult on us and can actually showcase the pickup
-	
-	
-	if (charging || emergency || charge == 0) { // do nothing
-		return;
-	}
-	if (charge < 0) { // dead
-		charge = 0;
-		emergency = true;
-		std::cout << "emergency\n" << std::endl;
-		return;
-	}
-	
-	if (nearestRecharge == NULL) {
-		GetNearestRecharge(*entities);
-	}
-	
-	if (this->GetAvailability()) { // no target, no moving
-		drone->Update(dt, scheduler); // try to initialize drone route
-		if (!(this->GetAvailability())) { // if successful (results in 1 move)
-			charge -= dt; // only drain when moving
-			if (nearestRecharge) { // if no recharge stations, attempt trip anyways
-				IEntity* passenger = drone->ReturnNearestEntity();
-				if (passenger) { // mulithread error? idk
-					double dist = TripDistance(passenger);
-					std::cout<<"TripDistance: " <<dist<<std::endl;
-					if ((dist / this->GetSpeed()) > (charge)) { // trip too long for battery
-						passenger->SetAvailability(true);
-						rechargeDest = nearestRecharge->GetPosition();
-						rechargeStrategy = new BeelineStrategy(this->GetPosition(), rechargeDest);
-					}
-				}
-			}
-		}
-	}
-	else if (rechargeStrategy) { // pathing to recharge. deleted and NULL when completed
-		if (nearIsMobile) { // initally assumed mobile, so always entered once
-			if ((nearestRecharge->GetPosition() - rechargeDest).Magnitude() <= 1.0) {
-				nearIsMobile = false;  // if nearest doesn't move, note as such to skip computation
-			}
-			rechargeDest = nearestRecharge->GetPosition();
-			delete rechargeStrategy;
-			rechargeStrategy = new BeelineStrategy(this->GetPosition(), rechargeDest);
-		}
-		rechargeStrategy->Move(this, dt);
-		charge -= dt;
-		if (rechargeStrategy->IsCompleted()) { // reached recharge, start charging
-			delete rechargeStrategy;
-			rechargeStrategy = NULL;
-			nearestRecharge->AddBattery(this);
+	if(charge > 30){
+		drone->SetAvailability(false);
+		if(drone->GetPosition().Distance(rechargeDest) < 1){
 			charging = true;
-			nearIsMobile = true;
+			Recharge(dt);
+		}
+		else{
+			nearestRecharge = GetNearestRecharge(*entities);
+			rechargeDest = nearestRecharge->GetPosition();
+			drone->SetDestination(rechargeDest);
+			position = drone->GetPosition();
+			drone->Update(dt,scheduler);
+			if(drone->GetPosition().Distance(position) > 0.1){
+				charge -= dt;
+				std::cout<<"charge: "<<charge<<std::endl;
+			}
 		}
 	}
-	else { // proceed trip as normal
-		drone->Update(dt, scheduler);
-		charge -= dt;
-		if (this->GetAvailability()) {
-			GetNearestRecharge(*entities);
+	else if(charge <= 0){
+		
+	}
+	else{
+		position = drone->GetPosition();
+		drone->Update(dt,scheduler);
+		if(drone->GetPosition().Distance(position) > 0.1){
+			charge -= dt;
+			std::cout<<"charge: "<<charge<<std::endl;
 		}
 	}
+	
+	
+	
 }
 
 void BatteryDecorator::Recharge(double amount) {

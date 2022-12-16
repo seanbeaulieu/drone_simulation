e200545291e069653c58a779118820fa731dcf22
beaul116 <beaul116@umn.edu>
2022-12-15 21:14:54 -0600

    rechargestation doxy

--- libs/transit/include/RechargeStation.h
+++ libs/transit/include/RechargeStation.h
@@ -19,22 +19,71 @@ class RechargeStation : public IEntity{
 
      ~RechargeStation() override = default;
 
+    /**
+     * @brief Returns the current position of the recharge station as a Vector3 object
+     *
+     * @return position, a Vector3
+     **/
      Vector3 GetPosition() const { return position; }
 
+    /**
+     * @brief Returns the current direction of the recharge station as a Vector3 object
+     * 
+     * @return direction, a Vector3
+     **/
      Vector3 GetDirection() const { return direction; }
      
+     /**
+     * @brief Returns the current destination of the recharge station as a Vector3 object
+     * 
+     * @return destination, a Vector3
+     **/
      Vector3 GetDestination() const { return destination; }
 
+    /**
+     * @brief Returns the current availability of the recharge station as a boolean value
+     * 
+     * @return available, a true or false boolean value
+     **/
      bool GetAvailability() const { return available; }
      
+     /**
+     * @brief Returns the details of the recharge station
+     * 
+     * @return details as a JsonObject
+     **/
      JsonObject GetDetails() const override;
 
+    /**
+     * @brief Sets the current availability
+     * 
+     * @param choice, a boolean value
+     * @return None
+     **/
      void SetAvailability(bool choice);
 
+    /**
+     * @brief Recharges the drone
+     *
+     * @return None
+     **/
      void recharge();
 
+    /**
+     * @brief Gets the current speed and returns it
+     * 
+     * @return speed, the current speed
+     **/
      float GetSpeed() const { return speed; }
      
+    /**
+     * @brief Updates the recharge station
+     * 
+     * @param dt, a double delta value
+     * @param scheduler, a vector array of IEntity ptrs
+     * 
+     * @return None
+     **/
      virtual void Update(double dt, std::vector<IEntity*> scheduler);  
 };
 
